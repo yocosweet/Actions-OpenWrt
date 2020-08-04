5f3c2910d5d407697a13db89a1ee71cf7f4d4d50
yocosweet <40546262+yocosweet@users.noreply.github.com>
2020-08-04 21:53:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,3 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git vernesong https://github.com/vernesong/OpenClash' feeds.conf.default
